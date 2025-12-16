8a6910874faf4b3b2e9aec7e8a92799fcd2e156b
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-15 22:58:24 -0300

    Criando o método de adicionar tarefa

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { TaskCardComponent } from "../task-card/task-card.component";
+import { Component, inject } from '@angular/core';
+import { TaskCardComponent } from '../task-card/task-card.component';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-list-section',
   imports: [TaskCardComponent],
   templateUrl: './task-list-section.component.html',
-  styleUrl: './task-list-section.component.css'
+  styleUrl: './task-list-section.component.css',
 })
 export class TaskListSectionComponent {
+  private readonly _taskService = inject(TaskService);
 
+  ngOnInit() {
+    this._taskService.todoTasks.subscribe((todoList) => {
+      console.log('Lista de TODOS: ', todoList);
+    });
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { ITask } from '../interfaces/task.interface';
+import { ITaskFormControls } from '../interfaces/task-form-controls.interface';
+import { TaskStatusEnum } from '../enums/task-status.enum';
+import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-timestamp';
 
 @Injectable({
   providedIn: 'root',
@@ -17,4 +20,17 @@ export class TaskService {
   //Tarefas a A fazer
   private doneTask$ = new BehaviorSubject<ITask[]>([]);
   readonly doneTasks = this.todoTask$.asObservable();
+
+  addTask(taskInfos: ITaskFormControls) {
+    const newTask: ITask = {
+      ...taskInfos,
+      status: TaskStatusEnum.TODO,
+      id: generateUniqueIdWithTimestamp(),
+      comments: [],
+    };
+
+    const currentList = this.todoTask$.value;
+
+    this.todoTask$.next([...currentList, newTask]);
+  }
 }
