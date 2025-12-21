9447dfffdcdda02b7925625921d767a478145c32
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-21 20:08:28 -0300

    Atualizando o nome e a descrição da tarefa

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, Input } from '@angular/core';
 import { ModalControllerService } from '../../services/modal-controller.service';
 import { ITask } from '../../interfaces/task.interface';
+import { TaskService } from '../../services/task.service';
+import { ITaskFormControls } from '../../interfaces/task-form-controls.interface';
 
 @Component({
   selector: 'app-task-card',
@@ -11,16 +13,24 @@ import { ITask } from '../../interfaces/task.interface';
 export class TaskCardComponent {
   @Input({ required: true }) task!: ITask;
 
+  private readonly _taskService = inject(TaskService);
   private readonly _modalControllerService = inject(ModalControllerService);
 
   openEditTaskModal() {
     const dialogRef = this._modalControllerService.openEditTaskModal({
-      name: 'Nome tarefa',
-      description: 'Descrição tarefa',
+      name: this.task.name,
+      description: this.task.description,
     });
 
-    dialogRef.closed.subscribe((taskForm) => {
-      console.log('Tarefa alterada: ', taskForm);
+    dialogRef.closed.subscribe((taskForm: any) => {
+      if (taskForm) {
+        this._taskService.updateTaskNameAndDescription(
+          this.task.id,
+          this.task.status,
+          taskForm.name,
+          taskForm.description,
+        );
+      }
     });
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -67,6 +67,30 @@ export class TaskService {
     }
   }
 
+  updateTaskNameAndDescription(
+    taskId: string,
+    taskCurrentStatus: TaskStatus,
+    newTaskName: string,
+    newTaskDescription: string,
+  ) {
+    const currentTaskList = this.getTaskListByStatus(taskCurrentStatus);
+    const currentTaskIndex = currentTaskList.value.findIndex(
+      (task) => task.id === taskId,
+    );
+
+    if (currentTaskIndex > -1) {
+      const updatedTaskList = [...currentTaskList.value];
+
+      updatedTaskList[currentTaskIndex] = {
+        ...updatedTaskList[currentTaskIndex],
+        name: newTaskName,
+        description: newTaskDescription,
+      };
+
+      currentTaskList.next(updatedTaskList);
+    }
+  }
+
   private getTaskListByStatus(taskStatus: TaskStatus) {
     const taskListObj = {
       [TaskStatusEnum.TODO]: this.todoTask$,
