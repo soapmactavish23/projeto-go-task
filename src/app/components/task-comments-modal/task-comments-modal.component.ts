212022359f49d4224285ba28800012162a0b42f2
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-24 20:45:53 -0300

    Criando e validando o FormControl de novo comentário

--- src/app/components/task-comments-modal/task-comments-modal.component.ts
+++ src/app/components/task-comments-modal/task-comments-modal.component.ts
@@ -1,12 +1,19 @@
 import { DIALOG_DATA } from '@angular/cdk/dialog';
 import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-comments-modal',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-comments-modal.component.html',
   styleUrl: './task-comments-modal.component.css',
 })
 export class TaskCommentsModalComponent {
+  commentControl = new FormControl('', [Validators.required]);
+
   readonly _task = inject(DIALOG_DATA);
+
+  onAddComment() {
+    console.log('Comentário: ', this.commentControl.value);
+  }
 }
