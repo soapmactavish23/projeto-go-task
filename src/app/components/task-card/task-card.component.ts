00b93537fdb507000b1cab241994d0a895dd44e4
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-24 20:37:39 -0300

    Renderizando os comentários da tarefa no modal

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -32,4 +32,12 @@ export class TaskCardComponent {
       }
     });
   }
+
+  openTaskCommentsModel() {
+    this.task.comments = [
+      { id: '123', description: 'Meu comentário 1' },
+      { id: '456', description: 'Meu comentário 2' },
+    ];
+    return this._modalControllerService.openTaskCommentsModal(this.task);
+  }
 }
--- src/app/components/task-comments-modal/task-comments-modal.component.ts
+++ src/app/components/task-comments-modal/task-comments-modal.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { DIALOG_DATA } from '@angular/cdk/dialog';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-task-comments-modal',
   imports: [],
   templateUrl: './task-comments-modal.component.html',
-  styleUrl: './task-comments-modal.component.css'
+  styleUrl: './task-comments-modal.component.css',
 })
 export class TaskCommentsModalComponent {
-
+  readonly _task = inject(DIALOG_DATA);
 }
